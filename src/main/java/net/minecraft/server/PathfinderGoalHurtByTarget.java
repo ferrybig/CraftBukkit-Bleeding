a4d02d5a0ddd7d23a2eb948ecfe5591dc0a94bc5
GJ <gjmcferrin@gmail.com>
2014-02-05 12:19:11 -0500

    [Bleeding] Call EntityTargetEvents for mobs attacking in a horde.

--- src/main/java/net/minecraft/server/PathfinderGoalHurtByTarget.java
+++ src/main/java/net/minecraft/server/PathfinderGoalHurtByTarget.java
@@ -32,7 +32,13 @@ public class PathfinderGoalHurtByTarget extends PathfinderGoalTarget {
                 EntityCreature entitycreature = (EntityCreature) iterator.next();
 
                 if (this.c != entitycreature && entitycreature.getGoalTarget() == null && !entitycreature.c(this.c.getLastDamager())) {
-                    entitycreature.setGoalTarget(this.c.getLastDamager());
+                    // CraftBukkit start - call EntityTargetEvent
+                    org.bukkit.event.entity.EntityTargetLivingEntityEvent event = org.bukkit.craftbukkit.event.CraftEventFactory.callEntityTargetLivingEvent(entitycreature, this.c.getLastDamager(), org.bukkit.event.entity.EntityTargetEvent.TargetReason.PIG_ZOMBIE_TARGET);
+                    if (event.isCancelled()) {
+                        continue;
+                    }
+                    entitycreature.setGoalTarget(event.getTarget() == null ? null : ((org.bukkit.craftbukkit.entity.CraftLivingEntity) event.getTarget()).getHandle());
+                    // CraftBukkit end
                 }
             }
         }
